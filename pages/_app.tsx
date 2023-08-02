6a07e7b3fcf79d0a84ba8293902b0e17102328d4
Big Core <127689088+BigCoreProject@users.noreply.github.com>
2023-08-02 12:46:02 +0300

    Update _app.tsx
    BigCore

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,17 @@
 import { ThirdwebProvider } from "@thirdweb-dev/react";
 import type { AppProps } from "next/app";
 import "../styles/globals.css";
+import { CoreBlockchain } from "@thirdweb-dev/chains"
+import { Navbar } from "../components/Navbar/Navbar";
 
 // This is the chain your dApp will work on.
-const activeChain = "mumbai";
+const activeChain = CoreBlockchain;
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThirdwebProvider
-      activeChain={activeChain}
-      clientId={process.env.NEXT_PUBLIC_TEMPLATE_CLIENT_ID}
-    >
+    <ThirdwebProvider activeChain={activeChain}>
+      {/* Render the navigation menu above each component */}
+      <Navbar />
       <Component {...pageProps} />
     </ThirdwebProvider>
   );
