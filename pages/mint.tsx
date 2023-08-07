d9693856d55a60674d9663360eb347c169722922
Big Core <127689088+BigCoreProject@users.noreply.github.com>
2023-08-08 01:38:40 +0300

    Update mint.tsx
    Mint Day-1 Started

--- pages/mint.tsx
+++ pages/mint.tsx
@@ -17,7 +17,11 @@ const Mint: NextPage = () => {
       </p>
        
       <p className={styles.explain}>
-        Price : 10 Core per item
+        Price (Whitelist) : 20 Core per item (Unlimited)
+      </p>
+
+      <p className={styles.explain}>
+        Price (Public) : 10 Core per item (Up to 3 per wallet)
       </p>
       
       <hr className={`${styles.smallDivider} ${styles.detailPageHr}`} />
